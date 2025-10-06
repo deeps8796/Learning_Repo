40b390b08c49021836f7432be4f4dc5caa448612
Deepika Moolchandani <93469843+deeps8796@users.noreply.github.com>
2025-10-06 03:56:47 +0000

    tests added

---
+++ test_sum.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <stdio.h>
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+void test_sum() {
+    assert(sum(2, 3) == 5);
+    assert(sum(-1, 1) == 0);
+    assert(sum(0, 0) == 0);
+    assert(sum(-5, -7) == -12);
+    assert(sum(100, 200) == 300);
+    printf("All tests passed!\n");
+}
+
+int main() {
+    test_sum();
+    return 0;
+}
